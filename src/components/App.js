0106b54ac4cdc130053b87eb39d72b34cec5495d
Joshua Yi <jryi@scu.edu>
2025-05-19 20:38:28 -0700

    create responsive appbar component

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,20 @@
 import React, { useState } from 'react';
 import Main from './Main';
+import ResponsiveAppBar from './ResponsiveAppBar';
 
 import { TOKEN_KEY } from '../constants';
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(
     localStorage.getItem(TOKEN_KEY) ? true : false
+    // true
   );
+
+  const logout = () => {
+    localStorage.removeItem(TOKEN_KEY);
+    setIsLoggedIn(false);
+  };
+
   const loggedIn = (token) => {
     if (token) {
       localStorage.setItem(TOKEN_KEY, token);
@@ -16,6 +24,10 @@ function App() {
 
   return (
     <div className="App">
+      <ResponsiveAppBar
+        isLoggedIn={isLoggedIn}
+        handleLogout={logout}
+      ></ResponsiveAppBar>
       <Main isLoggedIn={isLoggedIn} handleLoggedIn={loggedIn} />
     </div>
   );
