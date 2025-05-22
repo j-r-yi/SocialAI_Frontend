7e5eaa78b6836a88b2c2c80fffdf5b609cd2d78e
Joshua Yi <jryi@scu.edu>
2025-05-21 20:09:19 -0700

    update Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,78 @@
 import React from 'react';
+import { Form, Input, Button, message } from 'antd';
+import { UserOutlined, LockOutlined } from '@ant-design/icons';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+
+import '../styles/Login.css';
+import { BASE_URL } from '../constants';
 
 function Login(props) {
-  return <div>login</div>;
+  const { handleLoggedIn } = props;
+  const onFinish = (values) => {
+    const { username, password } = values;
+    const option = {
+      method: 'POST',
+      url: `${BASE_URL}/signin`,
+      data: {
+        username,
+        password,
+      },
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
+
+    // Axios returns a promise
+    axios(option)
+      .then((res) => {
+        if (res.status === 200) {
+          const { data } = res;
+          handleLoggedIn(data);
+          message.success('Logged In!');
+        }
+      })
+      .catch((err) => {
+        message.error('Login Failed!');
+      });
+  };
+  return (
+    <Form name="normal_login" className="login-form" onFinish={onFinish}>
+      <Form.Item
+        name="user"
+        rules={[{ required: true, message: 'Please input your username!' }]}
+      >
+        <Input
+          prefix={<UserOutlined className="site-form-item-icon" />}
+          placeholder="Username"
+        ></Input>
+      </Form.Item>
+
+      <Form.Item
+        name="password"
+        rules={[{ required: true, message: 'Please input your password!' }]}
+      >
+        <Input
+          prefix={<LockOutlined className="site-form-item-icon" />}
+          type="password"
+          placeholder="Password"
+        ></Input>
+      </Form.Item>
+
+      <Form.Item>
+        <Button
+          type="primary"
+          htmlType="submit"
+          className="login-form-button"
+          style={{ backgroundColor: 'black' }}
+        >
+          {' '}
+          Login
+        </Button>{' '}
+        Or <Link to="/register">Register Now!</Link>
+      </Form.Item>
+    </Form>
+  );
 }
 
 export default Login;
--- src/components/Register.js
+++ src/components/Register.js
@@ -1,7 +1,86 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { Form, Input, Button, message } from 'antd';
+import axios from 'axios';
+
+import { BASE_URL } from '../constants';
+
+// (mobile) responsiveness design
+const formItemLayout = {
+  labelCol: {
+    xs: { span: 24 },
+    sm: { span: 8 },
+  },
+  wrapperCol: {
+    xs: { span: 24 },
+    sm: { span: 16 },
+  },
+};
+
+const tailFormItemLayout = {
+  wrapperCol: {
+    xs: {
+      span: 16,
+      offset: 0,
+    },
+    sm: {
+      span: 16,
+      offset: 8,
+    },
+  },
+};
 
 function Register(props) {
-  return <div>register</div>;
+  const [form] = Form.userForm();
+  const navigate = useNavigate();
+
+  const onFinish = (values) => {
+    const { username, password } = values;
+    const opt = {
+      method: 'POST',
+      url: `${BASE_URL}/signup`,
+      data: {
+        username,
+        password,
+      },
+      headers: {
+        'content-type': 'application/json',
+      },
+    };
+    axios(opt)
+      .then((response) => {
+        if (response.status === 200) {
+          message.success('Registration Succeeded!');
+          navigate('/login');
+        }
+      })
+      .catch((error) => {
+        message.error('Signup Failed');
+      });
+  };
+
+  return (
+    <Form
+      {...formItemLayout}
+      form={form}
+      name="register"
+      onFinish={onFinish}
+      className="register"
+    >
+      <Form.Item
+        name="username"
+        label="Username"
+        rules={[
+          {
+            required: true,
+            message: 'Please input your username!',
+          },
+        ]}
+      >
+        <Input />
+      </Form.Item>
+    </Form>
+  );
 }
 
 export default Register;
