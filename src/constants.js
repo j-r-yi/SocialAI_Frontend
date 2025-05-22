bf2d077fbeef0320d1c43da518e3fa31d023adc1
Joshua Yi <jryi@scu.edu>
2025-05-21 20:53:07 -0700

    Stop tracking constants.js

--- src/constants.js
+++
@@ -1,8 +0,0 @@
-// JWT token
-export const TOKEN_KEY = 'token';
-export const BASE_URL = 'https://engaged-list-459002-h0.uw.r.appspot.com';
-export const SEARCH_KEY = {
-  all: 0,
-  keyword: 1,
-  user: 2,
-};
